686a4a0b0c7344da524cc2594573a35e8e6c529f
Artem <varivonchikartem@gmail.com>
2025-09-16 23:09:41 +0300

    refactor(structure): organize app folder structure

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,13 +4,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import "./index.scss";
+import { App } from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <BrowserRouter>
     <React.StrictMode>
-      <div>1s</div>
+      <App />
     </React.StrictMode>
   </BrowserRouter>
 );
